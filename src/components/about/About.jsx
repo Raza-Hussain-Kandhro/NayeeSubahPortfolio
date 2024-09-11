bfdcaebecc0cd52bb6e0c736645ceb39c899b673
raza <razahussainkandhro260@gmail.com>
2024-09-11 13:37:39 +0500

    update css and images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './about.css'
 import { FiFile } from 'react-icons/fi'
-import aboutSectionImage from '../../websitedesignimages/heropicture.jpg'
+import aboutSectionImage from '../../websitedesignimages/MyPicture.jpeg'
 import { BiBadge, BiHeadphone } from 'react-icons/bi'
 import { BsBag } from 'react-icons/bs'
 import Buttons from '../Buttons'
@@ -63,7 +63,7 @@ const About = () => {
           </div>
 
           <p className='about-section-bottom-right-middle'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sunt at quis a sit minus ut atque, architecto eius! Accusamus ratione esse dolore, suscipit dolorem deserunt rerum sed nesciunt culpa.
+          I am Raza Hussain, an experienced front-end developer with a strong background in building responsive and visually engaging web applications. I specialize in translating complex design ideas into functional, user-friendly interfaces using modern technologies. My focus is on delivering high-quality, scalable solutions that enhance user experience.
           </p>
 
           {/* <div className='herosection-name-container-button'>
--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -6,7 +6,7 @@ import { FaTwitter } from "react-icons/fa6";
 import { GiHand } from "react-icons/gi";
 import { FaTelegramPlane } from "react-icons/fa";
 
-import picture from '../../websitedesignimages/heropicture.jpg'
+import picture from '../../websitedesignimages/heropicture1.jpeg'
 import Buttons from '../Buttons';
 
 
@@ -53,13 +53,13 @@ const HeroSection = () => {
         <div className='herosection-name-container-top'>
 
           <div className='name-part'>
-            <h1>Raza hussain</h1>
+           <h1>Raza Hussain</h1>
             <span><GiHand /></span>
           </div>
 
 
           <h2>Frontend Developer</h2>
-          <p>Lorem ipsum,  sit amet consectetur adipisicing elit. Reiciendis est sequi, deleniti optio exercitationem, magni repellat totam rerum perspiciatis porro et tempora autem repudiandae iusto? Suscipit nesciunt illum consectetur quibusdam?</p>
+          <p>I'm a dedicated Front-End Developer skilled in HTML, CSS, JavaScript, and React. I create responsive, visually engaging websites with a focus on seamless user experiences. My work emphasizes clean code and smooth functionality across all platforms.</p>
         </div>
 
         <Buttons btnTitle={"Say Hello"} btnIcon={<FaTelegramPlane />} />
